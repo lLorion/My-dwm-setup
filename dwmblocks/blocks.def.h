4d584cb8628e3eaf7eac8fc71fba723d7b7c2c35
Lotion <164274510+lLotion@users.noreply.github.com>
2024-05-14 03:50:58 -0400

    Update blocks.def.h

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,21 +2,21 @@
 static const Block blocks[] = {
 /*Icon*//*Command*//*Update Interval*//*Update Signal*/
 
-	{" 󰍛 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	3,	0},
+	{" 󰍛 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		3,	0},
 
-	{" ", "date '+ %I:%M %p'",						5,	0},
+	{" ", "date '+ %I:%M %p'",							5,	0},
 
 	{" 󰔠  ",  "date '+%b %d (%a)'",						5,	0},
 
 	{"   ", "acpi | awk '{print $4}' | sed s/,//",				10,	0},
 	
-	{"", "curl 'wttr.in/<location>?format=2&nonce=$RANDOM'",			50,	0},
+	{"", "/home/arch/scripts/weather",						50,	0},
 
-	{" ", "acpi -t | awk '{print $4}' ",					5,	0},
+	{" ", "acpi -t | awk '{print $4}' ",						5,	0},
 
-	{ "󰻠 ", "/home/arch/scripts/core",					5,	0},
+	{ "󰻠 ", "/home/arch/scripts/core",						5,	0},
 
-	{"", "/home/arch/scripts/print",					0,	0},
+	{"", "/home/arch/scripts/print",						0,	0},
 
 };
 
